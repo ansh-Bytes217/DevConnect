3e8f93803791223278449107ac85a69cc3c29f1a
Ansh Malhotra <83686079+ansh-Bytes217@users.noreply.github.com>
2025-04-23 16:01:55 +0530

    Create Backend server.js

---
+++ Backend server.js
@@ -0,0 +1,29 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
+const connectionRoutes = require('./routes/connectionRoutes');
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/connections', connectionRoutes);
+
+// Connecting  to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('MongoDB connection error:', err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
